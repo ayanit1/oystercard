5414bc40881f22702ecb1d298bf8f6f101baf366
Albert <ayanit1@googlemail.com>
2017-02-22 15:03:18 +0000

    add station class with specs

---
+++ spec/feature_spec.rb
@@ -0,0 +1,16 @@
+# require 'oystercard'
+#
+# describe 'Oystercard features' do
+#   subject(:oystercard) { OysterCard.new }
+#
+#   context ':initialize' do
+#     it 'with a #balance of zero' do
+#       expect(oystercard.balance).to eq 0
+#     end
+#
+#     it 'with an empty array to #journey_hist' do
+#       expect(oystercard.journey_hist).to match_array([])
+#     end
+#
+#   end
+# end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,9 +1,13 @@
 require 'station'
 
 describe Station do
-  subject(:station) { described_class.new("Bond Street Station") }
+  subject(:station) { Station.new("Bond Street Station", 1) }
 
-  it 'responds to #name' do
-    expect(station.name).to eq "Bond Street Station"
+  it 'knows its name' do
+    expect(subject.name).to eq("Bond Street Station")
+  end
+
+  it 'knows its zone' do
+    expect(subject.zone).to eq(1)
   end
 end
