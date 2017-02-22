f0db4c34c8f4b65db5008fee4d111cd01f15b426
Albert <ayanit1@googlemail.com>
2017-02-22 15:14:34 +0000

    changes to feature specs

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -3,14 +3,12 @@
 # describe 'Oystercard features' do
 #   subject(:oystercard) { OysterCard.new }
 #
-#   context ':initialize' do
-#     it 'with a #balance of zero' do
-#       expect(oystercard.balance).to eq 0
-#     end
+#   it 'can top-up' do
+#   end
 #
-#     it 'with an empty array to #journey_hist' do
-#       expect(oystercard.journey_hist).to match_array([])
-#     end
+#   it 'can touch-in' do
+#   end
 #
+#   it 'can touch-out' do
 #   end
 # end
